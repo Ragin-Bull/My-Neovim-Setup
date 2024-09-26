6d479c213499d40d14a8afd36722a38205a841f3
Shatansh <shatansh.iitkgp@gmail.com>
2024-09-26 11:56:38 +0530

    initial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.cmd([[
+  set expandtab
+  set tabstop=2
+  set softtabstop=2
+  set shiftwidth=2
+  set number
+]])
+
+-- Leader key setup
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
